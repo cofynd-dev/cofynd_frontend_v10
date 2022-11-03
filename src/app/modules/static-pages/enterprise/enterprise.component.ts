3a0dba377ab1490c3aa64b94e1b1572c6f15eb3a
sachin-unyscape <sachin.kumar@unyscape.com>
2022-11-03 16:46:00 +0530

    enterprise page api binding

--- src/app/modules/static-pages/enterprise/enterprise.component.ts
+++ src/app/modules/static-pages/enterprise/enterprise.component.ts
@@ -1,15 +1,80 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { User } from '@core/models/user.model';
+import { UserService } from '@core/services/user.service';
+import { environment } from '@env/environment';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-enterprise',
   templateUrl: './enterprise.component.html',
-  styleUrls: ['./enterprise.component.scss']
+  styleUrls: ['./enterprise.component.scss'],
 })
 export class EnterpriseComponent implements OnInit {
+  constructor(
+    private userService: UserService,
+    private formBuilder: FormBuilder,
+    private toastrService: ToastrService,
+    private _formBuilder: FormBuilder,
+  ) {}
 
-  constructor() { }
+  submitted = false;
+  loading: boolean;
+  showSuccessMessage: boolean;
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  enterpriseFormGroup: FormGroup = this._formBuilder.group({
+    phone_number: ['', [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')]],
+    email: ['', [Validators.required, Validators.email]],
+    name: ['', Validators.required],
+    org_name: ['', Validators.required],
+    requirements: ['', Validators.required],
+  });
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.enterpriseFormGroup.controls;
   }
 
+  get emailid() {
+    return this.enterpriseFormGroup.controls;
+  }
+
+  get mobno() {
+    return this.enterpriseFormGroup.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.enterpriseFormGroup.invalid) {
+      return;
+    } else {
+      this.loading = true;
+      const object = {
+        user: {
+          phone_number: this.enterpriseFormGroup.controls['phone_number'].value,
+          email: this.enterpriseFormGroup.controls['email'].value,
+          name: this.enterpriseFormGroup.controls['name'].value,
+          org_name: this.enterpriseFormGroup.controls['org_name'].value,
+          requirements: this.enterpriseFormGroup.controls['requirements'].value,
+        },
+      };
+      this.userService.createLead(object).subscribe(
+        () => {
+          this.loading = false;
+          this.showSuccessMessage = true;
+          this.enterpriseFormGroup.reset();
+          this.submitted = false;
+        },
+        error => {
+          this.loading = false;
+          this.toastrService.error(error.message);
+        },
+      );
+    }
+  }
+
+  scrollToElement(element: HTMLElement) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
 }
