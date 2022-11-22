15087687f3c37cfe92907c2d2d93646fd9dc7c49
sachin-unyscape <sachin.kumar@unyscape.com>
2022-11-22 17:45:23 +0530

    build error resolved

--- src/app/modules/dynamic-modules/flat-space/flat-space-detail/flat-space-detail.component.ts
+++ src/app/modules/dynamic-modules/flat-space/flat-space-detail/flat-space-detail.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-flat-space-detail',
   templateUrl: './flat-space-detail.component.html',
-  styleUrls: ['./flat-space-detail.component.scss']
+  styleUrls: ['./flat-space-detail.component.scss'],
 })
 export class FlatSpaceDetailComponent implements OnInit {
+  show: any = false;
+  showamenity: any = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
