e6636b732591b3506be1b46b4a167370000aa802
sachin-unyscape <sachin.kumar@unyscape.com>
2022-12-20 13:32:31 +0530

    checking bug

--- src/app/shared/components/search-similar-location/search-similar-location.component.ts
+++ src/app/shared/components/search-similar-location/search-similar-location.component.ts
@@ -1,24 +1,30 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, Input, OnChanges, Renderer2 } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { generateSlug } from '@app/shared/utils';
 // import { MapsAPILoader } from '@core/map-api-loader/maps-api-loader';
 import { ToastrService } from 'ngx-toastr';
 import { Observable, Subscriber } from 'rxjs';
 import { script } from '../../../core/config/script';
+import { combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { WorkSpaceService } from '@app/core/services/workspace.service';
+
 
 @Component({
   selector: 'app-search-similar-location',
   templateUrl: './search-similar-location.component.html',
   styleUrls: ['./search-similar-location.component.scss'],
 })
+
 export class SearchSimilarLocationComponent implements OnChanges {
   @Input() country_names: string;
   @Input() cityName: string;
   @Input() popularLocationList = [];
   @Input() relativeUrl: string;
   @Input() isPageScrolled: boolean;
-  country: any = "India"
+  country: any;
+  cityResponse: any;
 
   constructor(
     private router: Router,
@@ -26,11 +32,28 @@ export class SearchSimilarLocationComponent implements OnChanges {
     private _renderer2: Renderer2,
     // private mapsAPILoader: MapsAPILoader,
     private toastrService: ToastrService,
+    private activatedRoute: ActivatedRoute,
+    private workSpaceService: WorkSpaceService,
   ) { }
+
+  ngOnInit() {
+    combineLatest(this.activatedRoute.url, this.activatedRoute.queryParams)
+      .pipe(map(results => ({ routeParams: results[0], queryParams: results[1] })))
+      .subscribe(results => {
+        let cityName = results.routeParams[0].path;
+        this.workSpaceService.getByCityName1(cityName.toLowerCase()).subscribe((res: any) => {
+          this.cityResponse = res.data;
+          localStorage.setItem('country_name', this.cityResponse.country.name.toLowerCase())
+          localStorage.setItem('country_id', this.cityResponse.country.id)
+          localStorage.setItem('city_name', this.cityResponse.name.toLowerCase())
+        })
+      });
+  }
 
   getSlug(location: string) {
     return generateSlug(location);
   }
+
   private getCurrentPosition(): any {
     return new Observable((observer: Subscriber<any>) => {
       if (navigator.geolocation) {
@@ -46,13 +69,13 @@ export class SearchSimilarLocationComponent implements OnChanges {
       }
     });
   }
+
   reRoute(location) {
     this.country = localStorage.getItem('country_name') ? localStorage.getItem('country_name') : this.country_names;
     if (this.relativeUrl === 'co-living' && this.country != 'india' && this.country != 'India' && this.country != 'INDIA') {
       const url = `/${this.country}/co-living/${this.cityName.toLowerCase().trim()}/${generateSlug(
         location.toLowerCase().trim(),
       )}`;
-
       this.router.navigate([url]);
     }
     if (this.relativeUrl === 'co-living' && (this.country == 'india' || this.country == 'India' || this.country == 'INDIA')) {
@@ -68,7 +91,6 @@ export class SearchSimilarLocationComponent implements OnChanges {
         this.cityName.toLowerCase().trim() +
         '/' +
         generateSlug(location.toLowerCase().trim());
-
       this.router.navigate([url]);
     }
     if (this.relativeUrl === 'office-space/rent') {
@@ -86,7 +108,6 @@ export class SearchSimilarLocationComponent implements OnChanges {
       }
       this.router.navigate([url]);
     }
-
     if (this.relativeUrl === 'coworking' && (this.country == 'india' || this.country == 'India' || this.country == 'INDIA')) {
       if (location === 'Near Me') {
         this.getCurrentPosition().subscribe((position: any) => {
@@ -116,5 +137,7 @@ export class SearchSimilarLocationComponent implements OnChanges {
     script.text = `${cityScript} `;
     this._renderer2.appendChild(this._document.head, script);
   }
+
   ngOnChanges(): void { }
+
 }
