5642714f07e5877738bcca8c7b6002bdf62ed51a
sachin-unyscape <sachin.kumar@unyscape.com>
2023-01-13 10:47:13 +0530

    new components

---
+++ src/app/shared/components/search-contact-us-text2/search-contact-us-text2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-contact-us-text2',
+  templateUrl: './search-contact-us-text2.component.html',
+  styleUrls: ['./search-contact-us-text2.component.scss']
+})
+export class SearchContactUsText2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
