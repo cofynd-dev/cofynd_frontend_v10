ae2f5cd90609fd4b410bdea79e587c150674c965
cofynd-dev <dev@cofynd.com>
2023-02-09 15:04:25 +0530

    city form

--- src/app/shared/components/city-page-enquire/city-page-enquire.component.ts
+++ src/app/shared/components/city-page-enquire/city-page-enquire.component.ts
@@ -306,7 +306,7 @@ export class CityPageEnquireComponent implements OnInit, OnChanges {
     //     formValues.living_space = this.workSpaceId;
     //     break;
     // }
-    this.btnLabel = 'Submitting Enquiry...';
+    this.btnLabel = 'Submitting...';
     this.userService.createEnquiry(formValues).subscribe(
       () => {
         this.loading = false;
