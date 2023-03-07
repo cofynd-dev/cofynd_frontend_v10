dccfc98992aeff0392b6db86bfa4ed76767ce4fb
sachin-unyscape <sachin.kumar@unyscape.com>
2023-03-07 13:58:24 +0530

    working on cities

--- src/app/modules/virtual-office/virtual-office-routing.module.ts
+++ src/app/modules/virtual-office/virtual-office-routing.module.ts
@@ -52,10 +52,99 @@ const routes: Routes = [
     path: 'chennai',
     component: VirtualOfficeCityComponent,
   },
+  {
+    path: 'jaipur',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'faridabad',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'bhubaneswar',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'coimbatore',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'chandigarh',
+    component: VirtualOfficeCityComponent,
+  },
+
+  {
+    path: 'goa',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'kolkata',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'lucknow',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'guwahati',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'bhopal',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'dehradun',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'ernakulam',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'calicut',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'ludhiana',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'mohali',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'nagpur',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'patna',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'raipur',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'surat',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'trivandrum',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'vadodara',
+    component: VirtualOfficeCityComponent,
+  },
+  {
+    path: 'visakhapatnam',
+    component: VirtualOfficeCityComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class VirtualOfficeRoutingModule {}
+export class VirtualOfficeRoutingModule { }
