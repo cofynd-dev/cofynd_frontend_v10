0a6c42584470eb402a9bd152cf55c0d5aa97a0c2
sachin-unyscape <sachin.kumar@unyscape.com>
2023-04-14 11:00:04 +0530

    search no result components

---
+++ src/app/shared/components/coliving-search-no-result/coliving-search-no-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coliving-search-no-result',
+  templateUrl: './coliving-search-no-result.component.html',
+  styleUrls: ['./coliving-search-no-result.component.scss']
+})
+export class ColivingSearchNoResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
