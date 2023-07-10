ef4837380b8cf328cd9a9a6395a1b29dfe47e54c
cofynd-dev <dev@cofynd.com>
2023-07-10 18:37:42 +0530

    about more bugs

--- src/app/shared/components/search-card/search-card.component.ts
+++ src/app/shared/components/search-card/search-card.component.ts
@@ -48,7 +48,7 @@ export class SearchCardComponent implements AfterViewInit {
   carouselTile: NguCarouselConfig = {
     grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
     slide: 1,
-    speed: 350,
+    speed: 600,
     point: {
       visible: true,
     },
