6635063f1c84bc1ce1e99f6513614022be5ca137
cofynd-dev <dev@cofynd.com>
2023-08-23 14:05:09 +0530

    add changes

--- src/app/modules/static-pages/marketing-pages/marketing-pages.component.ts
+++ src/app/modules/static-pages/marketing-pages/marketing-pages.component.ts
@@ -195,6 +195,24 @@ export class MarketingPagesComponent implements OnInit, AfterViewInit, OnDestroy
     },
   ]
 
+  coFyndAdvantages = [
+    {
+      icon: 'icons/work-spaces.svg',
+      title: '100,000+ Spaces',
+      description: 'Get access to 100,000+ spaces with easy availability and convenience anytime and anywhere. Space Search Made Simple with CoFynd',
+    },
+    {
+      icon: 'icons/brokerage-icon.svg',
+      title: 'Zero Brokerage',
+      description: "CoFynd is India’s fastest growing space discovery platform that doesn’t charge any brokerage from the customers.",
+    },
+    {
+      icon: 'icons/support.svg',
+      title: '100% Offline Support',
+      description: 'We provide complete offline support from choosing the best space, scheduling site visits, bookings and after sales.',
+    },
+  ];
+
   // coworkingPlan = [
   //   {
   //     icon:
