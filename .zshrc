d1da50b60af2e0fb0cfbf1688bfc1ce5d3a7dc31
Shivam Maheshwari <shivammaheshwari@Shivams-MacBook-Air.local>
2023-11-09 17:45:04 +0530

    shivam

--- .zshrc
+++ .zshrc
@@ -8,3 +8,7 @@ export NVM_DIR=~/.nvm
 source $(brew --prefix nvm)/nvm.sh
 export NVM_DIR=~/.nvm
 source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
+export NVM_DIR=~/.nvm
+source $(brew --prefix nvm)/nvm.sh
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   appUrl: 'https://cofynd.com',
   keys: {
     // GOOGLE_MAP: 'AIzaSyDrIBoEI-pQOvA87Q6UGXvKsyv1XBT5KEg',
-    GOOGLE_MAP: '',
+    // GOOGLE_MAP: '',
     LOCATIONIQ_MAP: 'pk.c0dc118d922d758a22955af83b95f5c4',
     RAZOR_PAY: 'rzp_live_1psf55EiMDZ4Vb',
     FACEBOOK_APP: '173373107448062',
